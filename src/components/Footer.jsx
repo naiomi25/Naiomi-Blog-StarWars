ba8a6c89f218fd16b63374ac4725acbc85c18b59
naiomi25 <naiomidiaz.her@gmail.com>
2025-05-10 09:40:38 +0000

    ajuste moviles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export const Footer = () => (
 			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i className="fa-solid fa-file"></i> for help.
 		</p>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa fa-heart text-danger" /> by{"Naiomi"}
+			
 		</p>
 	</footer>
 );
